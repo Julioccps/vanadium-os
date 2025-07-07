1d668a4a6c2b207d90951236374a11a9f4012925
Julioccps <julio191096@gmail.com>
2025-07-07 20:57:26 -0300

    Update kernel.s, Added typing capability to shell.s

--- kernel.s
+++ kernel.s
@@ -12,6 +12,9 @@ _start:
     mov cx, 15
     call read_line
     
+    call syscall_setup
+    call fs_init
+    
     jmp main_loop
 
 main_loop:
@@ -102,7 +105,7 @@ clear_buffer:
 parse_command:
     mov si, input_buffer
     mov di, cmd_exec
-    call compare_string
+    call compare_string 
     test ax, 1
     jz load_shell
 
@@ -114,6 +117,7 @@ parse_command:
 load_shell:
     cmp word [0x2000], 0
     je .no_shell
+    mov si, name        
     jmp 0x0000:0x2000
 .no_shell:
     mov si, no_shell_msg
@@ -122,25 +126,84 @@ load_shell:
     ret
 
 compare_string:
-.compare_loop:
-    mov al, [si]
-    cmp bl, [di]
+.loop:
+    mov al, byte [si]
+    mov bl, byte [di]
     cmp al, bl
-    jne .not_equal
-    test al, al
-    jnz .equal
+    jne .equal
+    cmp al, 0
+    je .not_equal      
     inc si
     inc di
-    jmp .compare_loop
+    jmp .loop
 .equal:
-    xor ax, ax
-    inc ax
+    mov ax, 1
     ret
 .not_equal:
     xor ax, ax
     ret
 
+;   ========== SYSCALLS ==========
+syscall_setup:
+    xor ax, ax
+    mov es, ax
+    mov word [es:0x80*4], syscall_handler
+    mov word [es:0x80*4+2], cs
+    ret
+syscall_handler:
+    cmp ah, 0x00
+    je sys_read
+    cmp ah, 0x01
+    je sys_write
+    cmp ah, 0x02
+    je sys_exit
+    cmp ah, 0x03
+    je sys_open
+    cmp ah, 0x04
+    je sys_close
+    iret
+sys_read:
+    mov ah, 0x00
+    int 0x16
+    iret
+
+sys_write:
+    call print_string
+    iret
+
+sys_exit:
+    jmp main_loop
+
+sys_open:
+;   =========== TODO ===========
+;   Handle File with file system
+;   ============================
+
+sys_close:
+;   ========== TODO ============
+;   Handle File with file system
+;   ============================
+
+
+;   ========= FILE SYSTEM =========
+fs_init:
+    mov cx, MAX_OPEN_FILES
+    mov di, file_descriptor_table
+.loop:
+    mov byte [di + FdInUseOffset], 0 
+    add di, FD_STRUCT_SIZE
+    loop .loop
+    ret
+
 ;   ========== DATA ==========
+MAX_OPEN_FILES equ 8
+FD_STRUCT_SIZE equ 10
+
+FdInUseOffset   equ 0 
+FdStartSector   equ 1 
+FdFileSize      equ 5 
+FdSeekPos       equ 9 
+
 entry_msg db "Kernel Working! Please enter your name: ", 0
 name times 16 db 0
 prompt db "> ", 0
@@ -150,5 +213,8 @@ no_shell_msg db "No shell loaded", 0
 
 input_buffer times 128 db 0
 
+file_descriptor_table:
+    times MAX_OPEN_FILES * FD_STRUCT_SIZE db 0
+    
 
-times 512 - ($ - $$) db 0
+times 1024 - ($ - $$) db 0
--- shell.s
+++ shell.s
@@ -2,44 +2,120 @@
 [ORG 0x2000]
 
 _start:
-    mov si, shell_msg
-    call print_string
+    mov bp, si      
+
+    mov si, welcome_msg
+    call sys_write
+    
+    mov si, bp      
+    call sys_write
+
     call new_line
-    jmp $
+
+    mov si, shell_msg
+    call sys_write
+    call new_line
+
+main_loop:
+    mov di, input_buffer
+    call clear_buffer
+    mov di, input_buffer
+    mov cx, 127
+    mov si, bp
+    call sys_write
+    mov si, prompt
+    call sys_write
+    call read_line
+    ; TODO: Adicionar aqui a lógica para interpretar o comando em 'input_buffer'
+    jmp main_loop
 
 ;   ========== FUNCTIONS ==========
-print_string:
-    lodsb
-    or al, al
-    jz .done
-    call print_char
-    jmp print_string
+
+read_line:
+    xor bx, bx 
+
+.key_loop:
+    call sys_read
+    cmp al, 0x0D
+    je .done
+    cmp al, 0x08
+    je .backspace
+
+    cmp bx, cx
+    jge .key_loop
+
+    mov [di + bx], al
+    inc bx
+    mov [char_buffer], al
+    mov si, char_buffer
+    call sys_write
+    jmp .key_loop
 
 .done:
+    mov byte [di + bx], 0
+    call new_line
     ret
 
-print_char:
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
+.backspace:
+    test bx, bx
+    jz .key_loop
+    dec bx
+    call print_backspace
+    jmp .key_loop
+
+clear_buffer:
+    xor al, al
+    rep stosb
+    ret
+
+sys_write:
+    mov ah, 0x01
+    int 0x80
+    ret
+
+sys_read:
+    mov ah, 0x00
+    int 0x80
+    ret
+
+sys_exit:
+    mov ah, 0x02
+    int 0x80
+    ret
+
+sys_open:
+    mov ah, 0x03
+    int 0x80
+    ret
+
+sys_close:
+    mov ah, 0x04
+    int 0x80
     ret
 
 new_line:
-    mov al, 0x0D
-    call print_char
-    mov al, 0x0A
-    call print_char
+    mov si, nl
+    call sys_write
     ret
 
-backspace:
-    mov al, 0x08
-    call print_char
-    mov al, 0x20
-    call print_char
-    mov al, 0x08
-    call print_char
+print_backspace:
+    mov si, char_backspace
+    call sys_write
+    mov si, char_space
+    call sys_write
+    mov si, char_backspace
+    call sys_write
     ret
 
 ;   ========== DATA ==========
+welcome_msg db "Welcome, ", 0
 shell_msg db "Shell Working!", 0
+nl db 0x0D, 0x0A, 0 
+prompt db "> ", 0
+input_buffer times 128 db 0
+char_buffer db 0, 0 
+
+char_backspace db 0x08, 0
+char_space db ' ', 0
+
 times 512 - ($ - $$) db 0
